c6d655ca492f199dd1c9ca7621d2f95cfe57416c
Jeronimas Dargis <jeronimasd@gmail.com>
2023-03-28 13:23:27 +0200

    feat: trying to access env variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+// Import all env vars from .env file
+require("dotenv").config();
+export const MY_SECRET_KEY = process.env.MY_SECRET_KEY;
+console.log(MY_SECRET_KEY);
+console.log("init");
 
-})
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({});
